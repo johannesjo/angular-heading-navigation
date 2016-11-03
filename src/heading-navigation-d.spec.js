8f604fc931cda533fda643c4e4b09669c6f089c5
Johannes Millan <johannes.millan@gmail.com>
2016-11-03 17:14:06 +0100

    code formatting and add first dummy unit test

--- src/heading-navigation-d.spec.js
+++ src/heading-navigation-d.spec.js
@@ -2,6 +2,10 @@
 
 describe('Directive: headingNav', function() {
 
-  // load the directive's module
-  beforeEach(module('angularHeadingNavigation'));
+    // load the directive's module
+    beforeEach(module('angularHeadingNavigation'));
+
+    it('some test', function() {
+        'string'.should.be.a('string');
+    });
 });
--- src/tpls.js
+++ src/tpls.js
@@ -1,8 +1,9 @@
-angular.module('angularHeadingNavigation').run(['$templateCache', function($templateCache) {
-  'use strict';
+angular.module('angularHeadingNavigation')
+    .run(['$templateCache', function($templateCache) {
+        'use strict';
 
-  $templateCache.put('heading-navigation-d.html',
-    "<ul class=\"heading-navigation\" data-ng-hide=\"devMode\"><li data-ng-repeat=\"item in tree\"><a data-ng-click=\"item.isExpanded=!item.isExpanded\"><strong>{{ item.text }}</strong></a><ul data-ng-if=\"item.sub\" data-uib-collapse=\"!item.isExpanded\"><li data-ng-repeat=\"subItem in item.sub\"><a href=\"#{{subItem.link}}\" data-ng-click=\"hideAllOthers()\">{{ subItem.text }}</a></li></ul></li></ul>"
-  );
+        $templateCache.put('heading-navigation-d.html',
+            "<ul class=\"heading-navigation\" data-ng-hide=\"devMode\"><li data-ng-repeat=\"item in tree\"><a data-ng-click=\"item.isExpanded=!item.isExpanded\"><strong>{{ item.text }}</strong></a><ul data-ng-if=\"item.sub\" data-uib-collapse=\"!item.isExpanded\"><li data-ng-repeat=\"subItem in item.sub\"><a href=\"#{{subItem.link}}\" data-ng-click=\"hideAllOthers()\">{{ subItem.text }}</a></li></ul></li></ul>"
+        );
 
-}]);
+    }]);
